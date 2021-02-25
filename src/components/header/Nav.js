6f1aec43086c87b1ffa118a8233632f375da9ff2
KoDaI YaNo <kodaiyano@yKD.local>
2021-02-25 22:14:52 +0900

    change icon to github

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,4 +1,4 @@
-import instagram from './instagram.svg';
+import github from './github.svg';
 import twitter from './twitter.svg';
 import youtube from './youtube.svg'
 
@@ -7,7 +7,7 @@ const Nav = () => {
         <div className="Navigation">
             <nav>
                 <ul>
-                    <li><a rel="noreferrer" href="https://www.instagram.com/ykd.jp/" target="_blank"><img src={instagram} className="instagram" alt="logo" /></a></li>
+                    <li><a rel="noreferrer" href="https://github.com/yKD1028" target="_blank"><img src={github} className="instagram" alt="logo" /></a></li>
                     <li><a rel="noreferrer" href="https://twitter.com/ykd_jp" target="_blank"><img src={twitter} className="twitter" alt="logo" /></a></li>
                     <li><a rel="noreferrer" href="https://youtu.be/iKyqZnDBBAY" target="_blank"><img src={youtube} className="youtube" alt="logo" /></a></li>
                 </ul>
